a168b98e55f5f35c5b7590c1387c91800a415e2c
Nikhil Patel <82591854+nikhilpatel4@users.noreply.github.com>
2023-09-02 18:30:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+a = int(input("Enter the first number (a): "))
+b = int(input("Enter the second number (b): "))
+
+#1. For floor division
+floor_division = a // b
+print("Floor Division (a // b):", floor_division)
+
+#2. For a power b
+power_result = a ** b
+print("a power b (a ** b):", power_result)
+
+#3. For Find out whether two variables have equal value or not - if yes then print True if they are not equal then print False
+
+if a == b:
+    print ("True")
+else:
+    print("False")
